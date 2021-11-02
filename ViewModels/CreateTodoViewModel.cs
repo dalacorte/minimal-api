bedbf881b26ac4d509f621be37e7a668d584e465
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-11-02 20:46:35 -0300

    view model

---
+++ ViewModels/CreateTodoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Api.Models;
+using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace Api.ViewModels
+{
+    public class CreateTodoViewModel : Notifiable<Notification>
+    {
+        public string Title { get; set; }
+
+        public Todo MapTo()
+        {
+            AddNotifications(new Contract<Notification>()
+                .Requires()
+                .IsNotNull(Title, "New Task")
+                .IsGreaterThan(Title, 5, "Grater than five"));
+
+            return new Todo(Guid.NewGuid(), Title, false);
+        }
+    }
+}
