84a47b9639097c55c82d17af3a1a421f59a08c3f
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-11-01 21:41:30 -0300

    appdbcontext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("DataSource=app.dv;Cache=Shared");
+    }
+}
